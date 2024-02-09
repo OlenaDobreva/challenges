6a7f35e1d5c6cc392e6d8dea4e6e8457eaa91910
OLENA DOBREVA <sectionone@ukr.net>
2024-02-09 16:07:46 +0100

    make calc

---
+++ js/index.js
@@ -0,0 +1,6 @@
+let mealCost = 25;
+let tipCostPercentage = 8;
+let tipAmount = (mealCost * tipCostPercentage) / 100;
+let totalCost = mealCost + tipAmount;
+console.log("tipAmount", tipAmount);
+console.log("The total cost of your bill is: ", totalCost);
