0d362eac85399a54cc5d126562a6d408c89ece7d
OLENA DOBREVA <sectionone@ukr.net>
2024-02-09 16:25:02 +0100

    savings calc

---
+++ savings.js
@@ -0,0 +1,8 @@
+let startingBalance = 999;
+let monthlyContribution = 48;
+let month = 12;
+let totalSavings = startingBalance + monthlyContribution * month;
+console.log(
+  "After 12 months of saving, you will have this sum in your account",
+  totalSavings
+);
