e33d19ac08c199ff52e19931586474e74b71f798
OLENA DOBREVA <sectionone@ukr.net>
2024-02-12 12:42:48 +0100

    solve darke mode

--- js-basics_dark-mode/js/index.js
+++ js-basics_dark-mode/js/index.js
@@ -1,3 +1,18 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const buttonDarkMode = document.querySelector('[data-js="dark-mode-button"]');
+buttonDarkMode.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+  bodyElement.classList.remove("light");
+});
+const buttonLightMode = document.querySelector('[data-js="light-mode-button"]');
+buttonLightMode.addEventListener("click", () => {
+  bodyElement.classList.add("light");
+  bodyElement.classList.remove("dark");
+});
+const buttonToggle = document.querySelector('[data-js="toggle-button"]');
+buttonToggle.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
