3b3bf27693f771d9eb7f37b7099dc1831cb22f64
OLENA DOBREVA <sectionone@ukr.net>
2024-02-16 10:59:19 +0100

    submit form

--- js-forms_handle-form-submit/js/index.js
+++ js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,13 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const userData = Object.fromEntries(formData);
+
+  console.log("userData", userData);
+
+  event.target.elements.firstName.focus();
+  event.target.reset();
+});
