8db63d9700d3f07148a1356fff50eee10f06a902
OLENA DOBREVA <sectionone@ukr.net>
2024-03-05 10:39:28 +0100

    props greeting

--- react-props_greeting/src/App.jsx
+++ react-props_greeting/src/App.jsx
@@ -1,5 +1,17 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Greeting name="Laya" />;
+}
+function Greeting({ name }) {
+  const coaches = [
+    "Jan",
+    "Sven",
+    "Klaus",
+    "Marcell",
+    "Gimena",
+    "Laya",
+    "Yorick",
+  ];
+  return <h1>Hello, {coaches.includes(name) ? "Coach! " : name}</h1>;
 }
