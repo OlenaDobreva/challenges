b66a877dd04dfb7fe2042a08cc746aa3d2dd1743
OLENA DOBREVA <sectionone@ukr.net>
2024-03-05 13:17:11 +0100

    download challenge

--- react-props_button/src/App.jsx
+++ react-props_button/src/App.jsx
@@ -1,5 +1,13 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Button onButtonclick={() => {console.log('click 1')}} color:'blue' disabled={true} text='Hello' />;
+  <Button onButtonclick={() => {console.log('click 2')}}  disabled={false} text='Hello'/>
+}
+function Button({color = 'green', disabled, text, onButtonclick}) {
+return <button onClick={onButtonclick}
+disabled={disabled}
+style={{backgroundColor: color, color: "gray"}}>
+  {text}
+</button>
 }
