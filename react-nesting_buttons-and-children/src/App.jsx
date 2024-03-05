f168e022491677f30c5aa2f439e7075d166b2dd0
OLENA DOBREVA <sectionone@ukr.net>
2024-03-05 13:34:20 +0100

    do button challenge

--- react-nesting_buttons-and-children/src/App.jsx
+++ react-nesting_buttons-and-children/src/App.jsx
@@ -3,18 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>Click me!</Button>
+      <Button>Eat me!</Button>
+      <Button>Drink me!</Button>
+      <Button>Bite me!</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
