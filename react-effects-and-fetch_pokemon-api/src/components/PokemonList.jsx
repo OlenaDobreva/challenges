38fcc26c577b9ad38481fbe16c81c3ee051f1c8d
OLENA DOBREVA <sectionone@ukr.net>
2024-03-12 10:56:27 +0100

    do pokemon chalenge

--- react-effects-and-fetch_pokemon-api/src/components/PokemonList.jsx
+++ react-effects-and-fetch_pokemon-api/src/components/PokemonList.jsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./PokemonList.css";
-
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
@@ -14,15 +13,17 @@ export default function PokemonList() {
       console.log(error);
     }
   }
+  useEffect(() => {
+    loadPokemon();
+  }, []);
 
   return (
     <main>
-      <button type="button" className="button" onClick={loadPokemon}>
-        Load Pokémon
-      </button>
       <ul>
         {pokemon.map(({ name }) => (
-          <li key={name} className="pokemon">{name}</li>
+          <li key={name} className="pokemon">
+            {name}
+          </li>
         ))}
       </ul>
     </main>
