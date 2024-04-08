db6a434a2e644af5912c228d60cdf7eca9d13f86
OLENA DOBREVA <sectionone@ukr.net>
2024-04-08 11:59:46 +0200

    add logic to form

--- backend-create_products/pages/api/products/index.js
+++ backend-create_products/pages/api/products/index.js
@@ -4,6 +4,19 @@ import Product from "../../../db/models/Product";
 export default async function handler(request, response) {
   await dbConnect();
 
+  // pages/api/index.js
+  if (request.method === "POST") {
+    try {
+      const productData = request.body;
+      await Product.create(productData);
+
+      response.status(201).json({ status: "Product created" });
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error: error.message });
+    }
+  }
+
   if (request.method === "GET") {
     const products = await Product.find();
     return response.status(200).json(products);
